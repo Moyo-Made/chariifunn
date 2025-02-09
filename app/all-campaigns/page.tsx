7a59ef3b4483425922861c808f694f6439aa3875
Moyo-Made <adegbitemoyomade2004@gmail.com>
2025-02-09 13:03:56 +0100

    Adds all campaign route

---
+++ app/all-campaigns/page.tsx
@@ -0,0 +1,12 @@
+import FundraisingCard from '@/components/Fundraising'
+import React from 'react'
+
+const page = () => {
+  return (
+	<div>
+	  <FundraisingCard />
+	</div>
+  )
+}
+
+export default page
